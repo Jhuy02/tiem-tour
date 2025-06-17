f1b8638bad4801a3d59f18938b42205b6558a6cd
Nguyen Xuan Phong <ngxphong03@gmail.com>
2025-06-17 17:31:32 +0700

    feat: compound banner

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,18 +1,9 @@
-import BlogCard from '@/app/_components/news-card'
-import RelatedBlogs from '@/app/_components/related-news'
-import React from 'react'
+import Banner from '@/app/_components/banner'
 
 export default function ContactPage() {
   return (
     <main className='relative'>
-      <BlogCard
-        className='w-[20.9375rem] shrink-0'
-        title=''
-        category=''
-        thumbSrc=''
-        date=''
-      />
-      <RelatedBlogs />
+      <Banner />
     </main>
   )
 }
--- src/app/_components/banner.tsx
+++ src/app/_components/banner.tsx
@@ -1,20 +1,28 @@
 'use client'
 import React from 'react'
 import {Swiper, SwiperSlide} from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+import Image from 'next/image'
 
-export default function Banner() {
+export default function Banner({data}: any) {
   return (
-    <section className='relative w-full h-[41.625rem]'>
+    <section className='relative w-full h-[37rem] overflow-hidden'>
+      <div className='absolute top-[9.5625rem] left-1/2 -translate-x-1/2 flex flex-col items-center z-[2]'>
+        <Image
+          width={300}
+          height={150}
+          alt=''
+          src='/images/background-heading'
+        />
+        <h1 className=''></h1>
+      </div>
       <Swiper className='w-full h-full'>
         <SwiperSlide className='w-full h-full'>Slide 1</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 2</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 3</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 4</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 5</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 6</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 7</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 8</SwiperSlide>
-        <SwiperSlide className='w-full h-full'>Slide 9</SwiperSlide>
+        <SwiperSlide className='w-full h-full'>Slide 1</SwiperSlide>
+        <SwiperSlide className='w-full h-full'>Slide 1</SwiperSlide>
+        <SwiperSlide className='w-full h-full'>Slide 1</SwiperSlide>
+        <SwiperSlide className='w-full h-full'>Slide 1</SwiperSlide>
       </Swiper>
     </section>
   )
