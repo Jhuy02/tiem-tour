abe723b373a23e6dcd98d2ece8f8d79419d8c652
RiverNguyen <ngiang1309@gmail.com>
2025-06-20 14:02:11 +0700

    Refactor GuildButton component to utilize getPathFromUrl for URL handling, enhancing link management consistency across the application.

--- src/app/_components/guild/_components/guild-button.tsx
+++ src/app/_components/guild/_components/guild-button.tsx
@@ -1,9 +1,10 @@
 import {BlueButton} from '@/components/blue-button'
+import {getPathFromUrl} from '@/hooks/useGetPathFromUrl'
 
 const GuildButton = ({button}: {button: {url: string; title: string}}) => (
   <div className='flex justify-center mt-[3rem] xsm:hidden'>
     <BlueButton
-      href={button.url}
+      href={getPathFromUrl(button.url)}
       textColor='#3b3943'
       borderColor='rgba(0,0,0, 0.12)'
       arrowColor='#3b3943'
